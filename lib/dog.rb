1197e3afdaa28e04b7d03090b73dfe60cf229da4
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-25 11:36:29 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+  def name=(dogs_name)
+    @this_is_dogs_name = dogs_name
+  end
+
+  def name
+    @this_is_dogs_name
+  end
+
+  def breed=(dogs_breed)
+    @this_is_dogs_breed = dogs_breed
+  end
+
+  def breed
+    @this_is_dogs_breed
+  end
