3ca0a0ed0f518bba99c585990d7c2e96815b0d2a
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-25 11:39:24 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+
+  def name=(persons_name)
+    @this_is_persons_name = persons_name
+  end
+  def name
+    @this_is_persons_name
+  end
+
+  def job=(persons_job)
+    @my_job = persons_job
+  end
+  def job
+    @my_job
