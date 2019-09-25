e506304966264df5448994e5fce249df47071b92
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-25 11:36:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,6 @@ class Dog
   def name=(dogs_name)
     @this_is_dogs_name = dogs_name
   end
-
   def name
     @this_is_dogs_name
   end
@@ -11,7 +10,6 @@ class Dog
   def breed=(dogs_breed)
     @this_is_dogs_breed = dogs_breed
   end
-
   def breed
     @this_is_dogs_breed
   end
